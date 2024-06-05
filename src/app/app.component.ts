976467beb226c2a885de3a5ea452b43d8e670383
eusen <eugene.snesarev@gmail.com>
2024-06-05 21:17:29 +0300

    added api changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello from {{ value }}</div>`,
+  template: `<div>{{ message }}</div>`,
 })
 export class AppComponent {
-  value = 'my first test Web Static App';
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http
+      .get('/api/message')
+      .subscribe((resp: any) => (this.message = resp.text));
+  }
 }
